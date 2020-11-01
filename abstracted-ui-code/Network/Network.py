23d3571b1c00d386e0da94580f129b35a4194c3e
salem almoatamed <s.almoatamed@uot.edu.ly>
2020-11-02 01:29:25 +0200

    adding put UI Code

---
+++ abstracted-ui-code/Network/Network.py
@@ -0,0 +1,296 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import sys
+import platform
+py3 = True
+
+class Toplevel1():
+    # the initialization function
+    def __init__(self):
+        self.root = tk.Tk()
+        '''This class configures and populates the toplevel window.
+           top is the toplevel containing window.'''
+        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
+        _fgcolor = '#000000'  # X11 color: 'black'
+        _compcolor = '#d9d9d9'  # X11 color: 'gray85'
+        _ana1color = '#d9d9d9'  # X11 color: 'gray85'
+        _ana2color = '#ececec'  # Closest X11 color: 'gray92'
+        self.style = ttk.Style()
+        if sys.platform == "win32":
+            self.style.theme_use('winnative')
+        self.style.configure('.', background=_bgcolor)
+        self.style.configure('.', foreground=_fgcolor)
+        self.style.configure('.', font="TkDefaultFont")
+        self.style.map('.', background=
+        [('selected', _compcolor), ('active', _ana2color)])
+
+        self.root.geometry("595x600+422+80")
+        self.root.title(self.device_name)
+        self.root.configure(background="#d9d9d9")
+        self.root.configure(highlightbackground="#d9d9d9")
+        self.root.configure(highlightcolor="black")
+
+        self.connection_parameter_frame = ttk.Labelframe(self.root)
+        self.connection_parameter_frame.place(relx=0.017, rely=0.02
+                                              , relheight=0.274, relwidth=0.941)
+        self.connection_parameter_frame.configure(relief='')
+        self.connection_parameter_frame.configure(text='''connection_parameter''')
+        self.connection_parameter_frame.configure(width=560)
+
+        self.port_description_frame = ttk.Labelframe(self.connection_parameter_frame)
+        self.port_description_frame.place(relx=0.018, rely=0.519, relheight=0.333
+                                          , relwidth=0.714, bordermode='ignore')
+        self.port_description_frame.configure(relief='')
+        self.port_description_frame.configure(text='''network_parameters''')
+        self.port_description_frame.configure(width=400)
+
+        self.port_entry = ttk.Entry(self.port_description_frame)
+        self.port_entry.place(relx=0.025, rely=0.444, relheight=0.467, relwidth=0.33, bordermode='ignore')
+        self.port_entry.configure(takefocus="")
+        self.port_entry.configure(cursor="ibeam")
+
+        self.ip_entry = ttk.Entry(self.port_description_frame)
+        self.ip_entry.place(relx=0.37, rely=0.444, relheight=0.467, relwidth=0.596, bordermode='ignore')
+        self.ip_entry.configure(takefocus="")
+        self.ip_entry.configure(cursor="ibeam")
+
+        self.connect_button = ttk.Button(self.connection_parameter_frame)
+        self.connect_button.place(relx=0.804, rely=0.34, height=25, width=76
+                                  , bordermode='ignore')
+        self.connect_button.configure(text='''connect''')
+        self.connect_button.configure(command=lambda e='': self.start1(e))
+
+        self.disconnect_button = ttk.Button(self.connection_parameter_frame)
+        self.disconnect_button.place(relx=0.804, rely=0.54, height=25, width=76
+                                     , bordermode='ignore')
+        self.disconnect_button.configure(takefocus="")
+        self.disconnect_button.configure(text='''disconnect''')
+        self.disconnect_button.configure(state='disable')
+        self.disconnect_button.configure(command=lambda e='': self.exit1(e))
+
+        self.exit_btn = ttk.Button(self.connection_parameter_frame)
+        self.exit_btn.place(relx=0.804, rely=0.74, height=25, width=76
+                            , bordermode='ignore')
+        self.exit_btn.configure(takefocus="")
+        self.exit_btn.configure(text='''exit''')
+        self.exit_btn.configure(command=lambda e='': self.exit2(e))
+
+        self.connection_state = ttk.Labelframe(self.root)
+        self.connection_state.place(relx=0.017, rely=0.304, relheight=0.68
+                                    , relwidth=0.941)
+        self.connection_state.configure(relief='')
+        self.connection_state.configure(text='''connection_state''')
+        self.connection_state.configure(width=560)
+
+        self.connection_state_text = ScrolledText(self.connection_state)
+        self.connection_state_text.place(relx=0.018, rely=0.06, relheight=0.928
+                                         , relwidth=0.966, bordermode='ignore')
+        self.connection_state_text.configure(background="white")
+        self.connection_state_text.configure(state='disabled')
+        self.connection_state_text.configure(font="TkTextFont")
+        self.connection_state_text.configure(foreground="black")
+        self.connection_state_text.configure(highlightbackground="#d9d9d9")
+        self.connection_state_text.configure(highlightcolor="black")
+        self.connection_state_text.configure(insertbackground="black")
+        self.connection_state_text.configure(insertborderwidth="3")
+        self.connection_state_text.configure(selectbackground="#c4c4c4")
+        self.connection_state_text.configure(selectforeground="black")
+        self.connection_state_text.configure(width=254)
+        self.connection_state_text.configure(wrap="none")
+
+        '''the following two statements used to initiate the port entry and file path entry'''
+        self.getIP()
+        self.ip_entry.insert(0, self.ip)
+        self.port_entry.insert(0, self.port)
+        self.path=str(os.path.expanduser('~/'))
+        os.chdir(self.path)
+        try:
+            os.mkdir(self.device_name)
+        except FileExistsError:
+            pass
+        os.chdir(self.path + self.device_name)
+        print(os.getcwd())
+        self.globalCode2localCode()
+
+        self.dbc()
+        try:
+            self.dbc('''
+                    CREATE TABLE sample(
+                    barcode unsigned int primary key,
+                    created_at datetime not null default current_timestamp
+                    )
+                ''')
+        except sqlite3.OperationalError as e:
+            print('already exists')
+            if str(e)[-6:] == 'exists':
+                pass
+            else:
+                raise sqlite3.OperationalError
+
+
+        try:
+            self.dbc('''
+                    CREATE TABLE counter(
+                    id unsigned int primary key not null default 1,
+                    count unsigned int not null default 1
+                    )
+                ''')
+            self.dbc('insert into counter(id,count) values(1,1);')
+        except sqlite3.OperationalError as e:
+            print('already exists')
+            if str(e)[-6:] == 'exists':
+                pass
+            else:
+                raise sqlite3.OperationalError
+
+
+
+        try:
+            self.dbc('''
+                    CREATE TABLE test(
+                    test_id unsigned int primary key not null,
+                    barcodeid varchar(30) not null,
+                    results varchar(20000) not null,
+                    uploadstate varchar(1) default 'n',
+                    created_at datetime not null default current_timestamp,
+                    FOREIGN KEY(barcodeid) REFERENCES user(barcode)
+                    );
+                ''')
+        except sqlite3.OperationalError as e:
+            print('already exists')
+            if str(e)[-6:] == 'exists':
+                pass
+            else:
+                raise sqlite3.OperationalError
+
+
+# The following code is added to facilitate the Scrolled widgets you specified.
+class AutoScroll(object):
+    '''Configure the scrollbars for a widget.'''
+
+    def __init__(self, master):
+        #  Rozen. Added the try-except clauses so that this class
+        #  could be used for scrolled entry widget for which vertical
+        #  scrolling is not supported. 5/7/14.
+        try:
+            vsb = ttk.Scrollbar(master, orient='vertical', command=self.yview)
+        except:
+            pass
+        hsb = ttk.Scrollbar(master, orient='horizontal', command=self.xview)
+
+        #self.configure(yscrollcommand=_autoscroll(vsb),
+        #    xscrollcommand=_autoscroll(hsb))
+        try:
+            self.configure(yscrollcommand=self._autoscroll(vsb))
+        except:
+            pass
+        self.configure(xscrollcommand=self._autoscroll(hsb))
+
+        self.grid(column=0, row=0, sticky='nsew')
+        try:
+            vsb.grid(column=1, row=0, sticky='ns')
+        except:
+            pass
+        hsb.grid(column=0, row=1, sticky='ew')
+
+        master.grid_columnconfigure(0, weight=1)
+        master.grid_rowconfigure(0, weight=1)
+
+        # Copy geometry methods of master  (taken from ScrolledText.py)
+        if py3:
+            methods = tk.Pack.__dict__.keys() | tk.Grid.__dict__.keys() \
+                  | tk.Place.__dict__.keys()
+        else:
+            methods = tk.Pack.__dict__.keys() + tk.Grid.__dict__.keys() \
+                  + tk.Place.__dict__.keys()
+
+        for meth in methods:
+            if meth[0] != '_' and meth not in ('config', 'configure'):
+                setattr(self, meth, getattr(master, meth))
+
+    @staticmethod
+    def _autoscroll(sbar):
+        '''Hide and show scrollbar as needed.'''
+        def wrapped(first, last):
+            first, last = float(first), float(last)
+            if first <= 0 and last >= 1:
+                sbar.grid_remove()
+            else:
+                sbar.grid()
+            sbar.set(first, last)
+        return wrapped
+
+    def __str__(self):
+        return str(self.master)
+
+
+def _create_container(func):
+    '''Creates a ttk Frame with a given master, and use this new frame to
+    place the scrollbars and the widget.'''
+    def wrapped(cls, master, **kw):
+        container = ttk.Frame(master)
+        container.bind('<Enter>', lambda e: _bound_to_mousewheel(e, container))
+        container.bind('<Leave>', lambda e: _unbound_to_mousewheel(e, container))
+        return func(cls, container, **kw)
+    return wrapped
+
+
+class ScrolledText(AutoScroll, tk.Text):
+    '''A standard Tkinter Text widget with scrollbars that will
+    automatically show/hide as needed.'''
+    @_create_container
+    def __init__(self, master, **kw):
+        tk.Text.__init__(self, master, **kw)
+        AutoScroll.__init__(self, master)
+
+
+def _bound_to_mousewheel(event, widget):
+    child = widget.winfo_children()[0]
+    if platform.system() == 'Windows' or platform.system() == 'Darwin':
+        child.bind_all('<MouseWheel>', lambda e: _on_mousewheel(e, child))
+        child.bind_all('<Shift-MouseWheel>', lambda e: _on_shiftmouse(e, child))
+    else:
+        child.bind_all('<Button-4>', lambda e: _on_mousewheel(e, child))
+        child.bind_all('<Button-5>', lambda e: _on_mousewheel(e, child))
+        child.bind_all('<Shift-Button-4>', lambda e: _on_shiftmouse(e, child))
+        child.bind_all('<Shift-Button-5>', lambda e: _on_shiftmouse(e, child))
+
+
+def _unbound_to_mousewheel(event, widget):
+    if platform.system() == 'Windows' or platform.system() == 'Darwin':
+        widget.unbind_all('<MouseWheel>')
+        widget.unbind_all('<Shift-MouseWheel>')
+    else:
+        widget.unbind_all('<Button-4>')
+        widget.unbind_all('<Button-5>')
+        widget.unbind_all('<Shift-Button-4>')
+        widget.unbind_all('<Shift-Button-5>')
+
+
+def _on_mousewheel(event, widget):
+    if platform.system() == 'Windows':
+        widget.yview_scroll(-1*int(event.delta/120),'units')
+    elif platform.system() == 'Darwin':
+        widget.yview_scroll(-1*int(event.delta),'units')
+    else:
+        if event.num == 4:
+            widget.yview_scroll(-1, 'units')
+        elif event.num == 5:
+            widget.yview_scroll(1, 'units')
+
+
+def _on_shiftmouse(event, widget):
+    if platform.system() == 'Windows':
+        widget.xview_scroll(-1*int(event.delta/120), 'units')
+    elif platform.system() == 'Darwin':
+        widget.xview_scroll(-1*int(event.delta), 'units')
+    else:
+        if event.num == 4:
+            widget.xview_scroll(-1, 'units')
+        elif event.num == 5:
+            widget.xview_scroll(1, 'units')
+
+#
+aia = Toplevel1()
+aia.root.mainloop()
+
